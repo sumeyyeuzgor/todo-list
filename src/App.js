3b5a66e27b87452ef2625c57cb3ba52cc17f58ed
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-02-06 14:47:48 +0300

    styling of todoItem components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { v4 as uuid } from "uuid";
+import { ReactComponent as EditIcon } from "./assets/edit-solid.svg";
+import { ReactComponent as DeleteIcon } from "./assets/trash-solid.svg";
+import { ReactComponent as PlusIcon } from "./assets/plus-square-regular.svg";
 
 function App() {
   const [todo, setTodo] = useState("");
@@ -27,12 +30,30 @@ function App() {
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
           />
-          <Button onClick={addTodo}>Add Todo</Button>
+          <Button
+            onClick={addTodo}
+            className="d-flex justify-content-center align-items-center gap-2"
+          >
+            <PlusIcon width={15} height={15} />
+            <span>Add Todo</span>
+          </Button>
         </div>
 
-        <div className="d-flex mt-3 flex-column w-75">
+        <div className="d-flex mt-3 flex-column w-75 gap-3">
           {todoList.map((todoItem) => (
-            <div key={todoItem.id}>{todoItem.todo}</div>
+            <div
+              key={todoItem.id}
+              className="d-flex justify-content-between align-items-center "
+            >
+              <div className="d-flex  justify-content-center align-items-center ">
+                <Form.Check type="checkbox" label="" />
+                <div>{todoItem.todo}</div>
+              </div>
+              <div className="d-flex gap-3  justify-content-center align-items-center ">
+                <EditIcon width={25} height={25} />
+                <DeleteIcon width={25} height={25} />
+              </div>
+            </div>
           ))}
         </div>
       </div>
