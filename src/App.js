0771e3be1c50968bda182bb88a52fbbae530de90
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-02-06 14:30:41 +0300

    add addTodo functionality on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([
+    "clean home",
+    "do homework",
+    "write code",
+  ]);
+  const addTodo = () => {
+    setTodoList((prevState) => [...prevState, todo]);
+    setTodo("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="d-flex justify-content-center w-100"
+      style={{ paddingTop: "15vh" }}
+    >
+      <div className="d-flex align-items-center flex-column w-75">
+        <h1>Todo List</h1>
+        <div className="d-flex justify-content-center mt-3 w-100">
+          <Form.Control
+            type="text"
+            placeholder="Todo"
+            className="w-50 me-5"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <Button onClick={addTodo}>Add Todo</Button>
+        </div>
+
+        <div className="d-flex mt-3 flex-column w-75">
+          {todoList.map((todo, index) => (
+            <div key={index}>{todo}</div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
